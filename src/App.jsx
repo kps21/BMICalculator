7b3bbda02ba24445ad5b2ed2d046facb7ab5e9f6
Sivakumar Ponnambalam <kps15695@gmail.com>
2024-06-18 23:21:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmi, setBmi] = useState(null);
+  const [bmiStatus, setBmiStatus] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const calculateBmi = () => {
+    const isValidHeight = /^\d+$/.test(height);
+    const isValidWeight = /^\d+$/.test(weight);
+    if (isValidHeight && isValidWeight) {
+      const heightInMeters = height / 100;
+      const bmiValue = weight / (heightInMeters * heightInMeters);
+      setBmi(bmiValue.toFixed(2));
+      setErrorMessage("");
+      if (bmiValue < 18.5) {
+        setBmiStatus("Under Weight");
+      } else if (bmiValue >= 18.5 && bmiValue < 24.9) {
+        setBmiStatus("Normal Weight");
+      } else if (bmiValue >= 25 && bmiValue < 29.9) {
+        setBmiStatus("Over Weight");
+      } else {
+        setBmiStatus("Obese");
+      }
+    } else {
+      setBmi(null);
+      setBmiStatus("");
+      setErrorMessage("Please enter valid number value for height and weight");
+    }
+  };
+  const clearAll = () => {
+    setHeight("");
+    setWeight("");
+    setBmi(null);
+    setBmiStatus("");
+  };
+
+  return (
+    <>
+      <div className="bmi-calculator">
+        <div className="box"></div>
+        <div className="data">
+          <h1>BMI Calculator</h1>
+          {errorMessage && <p className="error">{errorMessage}</p>}
+          <div className="input-container">
+            <label htmlFor="height">Height (cm)</label>
+            <input
+              type="text"
+              id="height"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="weight">Weight (kg)</label>
+            <input
+              type="text"
+              id="weight"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </div>
+          <button onClick={calculateBmi}>Calculate BMI</button>
+          <button onClick={clearAll}>Clear</button>
+          {bmi !== null && (
+            <div className="result">
+              <p>Yout BMI is: {bmi}</p>
+              <p>BMI status: {bmiStatus}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
